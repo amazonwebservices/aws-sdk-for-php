af0a8f8be7ab74432e1fcaa56b6579db7b557d84
Ryan Parman <ryan@ryanparman.com>
2011-06-21 14:26:39 -0700

    1.3.5 release. https://aws.amazon.com/releasenotes/PHP/1251631559033530

--- sdk.class.php
+++ sdk.class.php
@@ -102,9 +102,9 @@ function __aws_sdk_ua_callback()
 // INTERMEDIARY CONSTANTS
 
 define('CFRUNTIME_NAME', 'aws-sdk-php');
-define('CFRUNTIME_VERSION', '1.3.4');
+define('CFRUNTIME_VERSION', '1.3.5');
 // define('CFRUNTIME_BUILD', gmdate('YmdHis', filemtime(__FILE__))); // @todo: Hardcode for release.
-define('CFRUNTIME_BUILD', '20110607180731');
+define('CFRUNTIME_BUILD', '20110621180731');
 define('CFRUNTIME_USERAGENT', CFRUNTIME_NAME . '/' . CFRUNTIME_VERSION . ' PHP/' . PHP_VERSION . ' ' . php_uname('s') . '/' . php_uname('r') . ' Arch/' . php_uname('m') . ' SAPI/' . php_sapi_name() . ' Integer/' . PHP_INT_MAX . ' Build/' . CFRUNTIME_BUILD . __aws_sdk_ua_callback());
 
 
