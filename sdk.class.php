281c2bbc33a403dd99ee7a4444d63e021bc25895
amazonwebservices <aws-dr-tools@amazon.com>
2011-10-12 15:46:54 -0700

    1.4.4 release. https://aws.amazon.com/releasenotes/PHP/2759956528299919

--- sdk.class.php
+++ sdk.class.php
@@ -125,9 +125,9 @@ function __aws_sdk_ua_callback()
 // INTERMEDIARY CONSTANTS
 
 define('CFRUNTIME_NAME', 'aws-sdk-php');
-define('CFRUNTIME_VERSION', '1.4.3');
+define('CFRUNTIME_VERSION', '1.4.4');
 // define('CFRUNTIME_BUILD', gmdate('YmdHis', filemtime(__FILE__))); // @todo: Hardcode for release.
-define('CFRUNTIME_BUILD', '20110930191027');
+define('CFRUNTIME_BUILD', '20111012191027');
 define('CFRUNTIME_USERAGENT', CFRUNTIME_NAME . '/' . CFRUNTIME_VERSION . ' PHP/' . PHP_VERSION . ' ' . str_replace(' ', '_', php_uname('s')) . '/' . str_replace(' ', '_', php_uname('r')) . ' Arch/' . php_uname('m') . ' SAPI/' . php_sapi_name() . ' Integer/' . PHP_INT_MAX . ' Build/' . CFRUNTIME_BUILD . __aws_sdk_ua_callback());
 
 
@@ -1574,6 +1574,8 @@ class CFLoader
 		{
 			require_once($path);
 		}
+
+		return true;
 	}
 }
 
